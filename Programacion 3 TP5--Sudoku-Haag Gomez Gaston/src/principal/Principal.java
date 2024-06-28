4b598514e84259008e1910c1945915458f3b6ccb
GastonIvanHaagGomez <gasel88@hotmail.com>
2024-06-28 08:38:26 -0300

    Add files via upload

---
+++ Programacion 3 TP5--Sudoku-Haag Gomez Gaston/src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import modelo.Sudoku;
+import vista.PanelFrontalSudoku;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        PanelFrontalSudoku sudoku = new PanelFrontalSudoku();
+        sudoku.setVisible(true);        
+    }
+}
